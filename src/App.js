9aba57619a71b6ce1e001be293de270804077f19
shubham kushwaha <shubhkush1234@gmail.com>
2021-09-04 15:31:45 +0530

    ksdsd

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import axios from 'axios';
 function App() {
 
   const [results, setResults] = useState([]);
+  const abc = [];
 
   useEffect(() => {
     axios.get("http://hn.algolia.com/api/v1/search?query=reacthooks")
       .then(response => {
         console.log(response.data);
         setResults(response.data.hits);
-      }); 
+      });
   });
 
 
