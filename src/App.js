1c86709a96f9b61b495fc9a6c5699441a7b47b04
shubham kushwaha <shubhkush1234@gmail.com>
2021-09-06 21:25:16 +0530

    added clear btn and refs

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
 function App() {
 
   const [results, setResults] = useState([]);
-  const [query, setQuery] = useState("react hooks")
-
+  const [query, setQuery] = useState("react hooks");
+  const searchInputRef = useRef();
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     getResults();
     },[]);
 
 
   const getResults = async () => {
+    setLoading(true);
      const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`)
       .then(response => {
         console.log(response.data);
         setResults(response.data.hits);
+
+        setLoading(false);
   })
 }
 
 const handleSubmit = (event) => {
-  alert("submitted")
+  // alert("submitted");
   event.preventDefault();
   getResults();
 }
 
-
+const handleClearSearch = () => {
+  setQuery("");
+  searchInputRef.current.focus();
+};
   return (
     <div className="App" >
-      <form onSubmit={e => handleSubmit(e)}>
-        <input type="text" onChange={(event) => setQuery(event.target.value)} value={query}></input>
-        <button type="submit">search</button>
-      </form>
-      
-      {results.map(result => (
-        <li>
-         <a href={result.url}>{result.title}</a>
-        </li>
-      ))}
+      {loading ? (<div>Loading results...</div>):
+        (<form onSubmit={e => handleSubmit(e)}>
+          <input type="text" ref={searchInputRef} onChange={(event) => setQuery(event.target.value)} value={query}></input>
+          <button type="submit">search</button>
+          <button type="button" onClick={handleClearSearch}>Clear</button>
+        </form>
+        )
+        
+      }   
+        {results.map(result => (
+          <li>
+            <a href={result.url}>{result.title}</a>
+          </li>
+        ))
+      }
     </div>
   );
 }
